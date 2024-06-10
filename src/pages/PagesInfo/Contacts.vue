603c2dba9ffd217bc9084c1c51a2ae86e606573b
Alexandra <130577060+Lexa3940@users.noreply.github.com>
2024-06-10 19:36:48 +0300

    Update Contacts.vue

--- src/pages/PagesInfo/Contacts.vue
+++ src/pages/PagesInfo/Contacts.vue
@@ -3,7 +3,7 @@
   <p class="pt-4 text-lg" style="font-size: 20px">Адреса наших магазинов:</p>
   <p class="pt-4 text-lg" style="font-size: 20px">г. Черкесск, ул. Парковая 21</p>
   <p class="pt-4 text-lg" style="font-size: 20px">г. Черкесск, ул. Космонавтов 13</p>
-  <img class="pt-12 pb-12" src="shop.png" alt="Наши магазины" />
+  <img class="pt-12 pb-12" src="/shop.png" alt="Наши магазины" />
 </template>
 <script>
 export default {
