bae2621aadb9eae6016c7976da9387d4c244eb2c
Alexandra <130577060+Lexa3940@users.noreply.github.com>
2024-06-10 19:37:59 +0300

    Update Payment.vue

--- src/pages/PagesInfo/Payment.vue
+++ src/pages/PagesInfo/Payment.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="page-content">
-    <img class="pt-12 pb-12" src="pay.jpg" />
+    <img class="pt-12 pb-12" src="/pay.jpg" />
     <p class="pt-4 text-lg" style="font-size: 23px">Способы оплаты:</p>
     <p class="pt-4 text-lg" style="font-size: 23px">
       — Банковской картой или наличными при самовывозе
