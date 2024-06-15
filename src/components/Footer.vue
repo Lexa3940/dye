c48261c755bb196a046ba2aeb73c38352759e969
Alexandra <130577060+Lexa3940@users.noreply.github.com>
2024-06-15 22:00:43 +0300

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,17 @@
 <template>
   <footer class="footer">
     <div class="container">
-      <a href="https://api.whatsapp.com/send?phone=89996384073" target="_blank">
-        <img height="50" width="50" src="/whats.jpg" alt="Whatsapp" />
-      </a>
-      <a href="https://t.me/Cloud_020" target="_blank" rel="noopener noreferrer">
-        <img height="50" width="50" src="/tg2.jpg" alt="Telegram" />
-      </a>
+      <div class="left-side">
+        <span>Цветы 24</span>
+      </div>
+      <div class="right-side">
+        <a href="https://api.whatsapp.com/send?phone=89996384073" target="_blank">
+          <img height="30" width="30" src="/whats.jpg" alt="Whatsapp" />
+        </a>
+        <a href="https://t.me/Cloud_020" target="_blank" rel="noopener noreferrer">
+          <img height="30" width="30" src="/tg2.jpg" alt="Telegram" />
+        </a>
+      </div>
     </div>
   </footer>
 </template>
@@ -19,18 +24,34 @@ export default {
 
 <style scoped>
 .footer {
-  background-color: #FFDEAD;
-  padding: 20px 0;
-  text-align: center;
+  background-color: #FFDAB9;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .container {
+  width: 100%;
   display: flex;
-  justify-content: center;
-  gap: 20px;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.left-side {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+
+.right-side {
+  display: flex;
+  gap: 10px;
 }
 
 img {
   cursor: pointer;
+  height: 30px;
+  width: 30px;
 }
 </style>
+
